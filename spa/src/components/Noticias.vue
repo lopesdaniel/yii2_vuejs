4d962f526d84bc34d4f12585b1787a7a9fed29e1
Daniel Lopes <viana.daniel02@gmail.com>
2019-02-24 22:31:44 -0300

    Feito componente de Noticias

---
+++ spa/src/components/Noticias.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <h1 class="display-3">Cadastro de Notícias</h1>
+    <div class="row">
+      <div class="col-md-5">
+        <form action></form>
+      </div>
+      <div class="col-md-7">
+        <table class="table table-hover table-striped">
+          <thead>
+            <tr>
+              <th>Cód</th>
+              <th>Título</th>
+              <th>Categorias</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Noticias"
+};
+</script>
+
+<style scoped>
+</style>
+
